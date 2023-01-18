dd619f67ac08cf73488d1c2d84630c0092a091e0
Papa Cheikh Gningue <papacheikhgningue@esp.sn>
2023-01-18 14:55:16 +0000

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,58 @@
 function addTask() {
+    let inputElement = document.getElementById('task');
+    let inputContent = inputElement.value;
+
+    let new_item = document.createElement('li');
+
+    let new_item_label = document.createElement('a');
+    new_item_label.innerHTML = inputContent;
+
+    new_item.appendChild(new_item_label);
+
+    let list = document.getElementById('listTask');
+    list.appendChild(new_item);
+
+    inputElement.value = '';
+    inputElement.focus();
+
+    // themeroller.jquerymobile.com
+
+    let task_done = document.getElementById('Terminated');
+
+    $(new_item).on('swiperight', function () {
+        //$this.toggleClass("termine");
+        if (new_item.parentElement.id == 'listTask') {
+            task_done.appendChild(new_item);
+        }
+        else {
+            list.appendChild(new_item);
+        }
+    });
+
+    $(new_item).on('swipeleft', function () {
+        // this.remove();
+        $(new_item).slideUp('slow', function () {
+            this.remove();
+        });
+    });
+
+
+
+    $(list).listview('refresh');
+}
+
+function resetList() {
+    let listTask = document.getElementById('listTask');
+
+    while (listTask.firstChild) {
+        listTask.removeChild(listTask.firstChild);
+    }
+}
+
+
+
+
+/* function addTask() {
     var inputElement = document.getElementById('task');
     var inputContent = inputElement.value;
     var newListItem = document.createElement('li');
@@ -20,33 +74,34 @@ function resetList() {
     /* var listTask = document.getElementById('listTask');
     while (listTask.firstChild) {
         listTask.removeChild(listTask.firstChild);
-    } */
+    } *//*
 }
 let startX;
 function touchStart(event) {
-    startX = event.touches[0].clientX;
+startX = event.touches[0].clientX;
 }
 
 function touchEnd(event) {
-    const deltaX = event.changedTouches[0].clientX - startX;
-    if (Math.abs(deltaX) > 150) {
-        this.remove();
-    } else if (Math.abs(deltaX) > 50) {
-        if (Math.abs(deltaX) < 150) {
-            if (deltaX > 0) {
-                var list = document.getElementById('Terminated');
-                list.appendChild(listTask.firstChild);
+const deltaX = event.changedTouches[0].clientX - startX;
+if (Math.abs(deltaX) > 150) {
+this.remove();
+} else if (Math.abs(deltaX) > 50) {
+if (Math.abs(deltaX) < 150) {
+if (deltaX > 0) {
+var list = document.getElementById('Terminated');
+list.appendChild(listTask.firstChild);
 
-                newListItem.addEventListener('touchstart', touchStart);
-                newListItem.addEventListener('touchend', touchEnd);
-            } else {
-                var list = document.getElementById('listTask');
-                list.appendChild(Terminated.firstChild);
+newListItem.addEventListener('touchstart', touchStart);
+newListItem.addEventListener('touchend', touchEnd);
+} else {
+var list = document.getElementById('listTask');
+list.appendChild(Terminated.firstChild);
 
-                newListItem.addEventListener('touchstart', touchStart);
-                newListItem.addEventListener('touchend', touchEnd);
-            }
-        }
-    }
+newListItem.addEventListener('touchstart', touchStart);
+newListItem.addEventListener('touchend', touchEnd);
+}
+}
+}
 
 }
+*/
